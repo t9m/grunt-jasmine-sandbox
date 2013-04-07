63f7530e1972ac0ef7f0d3f24c6852991bbe5a05
t9m <dots64if@gmail.com>
2013-04-07 20:26:34 +0900

    Modify gitignore

--- spec/testSpec.js
+++
@@ -1,7 +0,0 @@
-describe('test', function() {
-  return it('should return argument', function() {
-    expect(test(1)).toEqual(1);
-    expect(test('hoge')).toEqual('hoge');
-    return expect(test(2)).not.toEqual(1);
-  });
-});
